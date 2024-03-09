71de4d84ebc6ec026c49cf464f342d81332cfd7d
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-03-09 20:24:47 +0100

    replace all svgs with html code

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,11 @@
 const output = document.getElementById("form__output-file-hash");
-const outputLabel = document.getElementById("form__label-output-file-hash")
+const outputLabel = document.getElementById("form__label-output-file-hash");
 const file = document.getElementById("form__input-file");
 const fileList = document.getElementById("form__wrapper-file-list");
 const numOfFiles = document.getElementById("form__number-of-files");
-const processingAnimation = document.getElementById("form__svg-animation-loading");
+const processingAnimation = document.getElementById(
+  "form__svg-animation-loading"
+);
 const dropbox = document.getElementById("form__wrapper-drop-zone");
 const selectHash = document.getElementById("form__select-hash");
 
@@ -36,7 +38,6 @@ function drop(e) {
   hashFilesAndUpdateOutput();
 }
 
-
 async function showProcessingAnimation() {
   processingAnimation.style.display = "block";
 }
@@ -67,7 +68,7 @@ function numberOfFiles() {
 
   fileList.innerHTML = "";
 
-  if (numFiles==1){
+  if (numFiles == 1) {
     numOfFiles.textContent = `${numFiles} File Selected`;
   } else {
     numOfFiles.textContent = `${numFiles} Files Selected`;
@@ -75,10 +76,8 @@ function numberOfFiles() {
   }
 }
 
-
-
 function listofFiles() {
-  for (f of file.files){
+  for (f of file.files) {
     let listItem = document.createElement("li");
     listItem.style.display = "flex";
     listItem.style.justifyContent = "space-between";
@@ -89,40 +88,34 @@ function listofFiles() {
     let fileName = f.name;
 
     // Approximate to the closest prefixed unit
-    const units = [
-      "B",
-      "KiB",
-      "MiB",
-      "GiB",
-      "TiB",
-      "PiB",
-      "EiB",
-      "ZiB",
-      "YiB",
-    ];
+    const units = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"];
 
     const exponent = Math.min(
       Math.floor(Math.log(f.size) / Math.log(1000)),
-      units.length - 1,
+      units.length - 1
     );
     const approx = f.size / 1000 ** exponent;
     const fileSize =
       exponent === 0
         ? `${f.size} bytes`
-        : `${approx.toFixed(3)} ${
-            units[exponent]
-          }`;
-          // } (${f.size} bytes)`;
+        : `${approx.toFixed(3)} ${units[exponent]}`;
+    // } (${f.size} bytes)`;
 
-    listItem.innerHTML=`<div class="form__wrapper-fileIcon-name"><i class="fa-solid fa-file-lines"></i><p>${fileName}</p></div><p>${fileSize}</p>`;
+    listItem.innerHTML = `
+    <div class="form__wrapper-fileIcon-name">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 form__icon-file">
+        <path fill-rule="evenodd" d="M5.625 1.5c-1.036 0-1.875.84-1.875 1.875v17.25c0 1.035.84 1.875 1.875 1.875h12.75c1.035 0 1.875-.84 1.875-1.875V12.75A3.75 3.75 0 0 0 16.5 9h-1.875a1.875 1.875 0 0 1-1.875-1.875V5.25A3.75 3.75 0 0 0 9 1.5H5.625ZM7.5 15a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 7.5 15Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H8.25Z" clip-rule="evenodd" />
+        <path d="M12.971 1.816A5.23 5.23 0 0 1 14.25 5.25v1.875c0 .207.168.375.375.375H16.5a5.23 5.23 0 0 1 3.434 1.279 9.768 9.768 0 0 0-6.963-6.963Z" />
+      </svg>
+      <p>${fileName}</p>
+    </div>
+    <p>${fileSize}</p>`;
 
     fileList.appendChild(listItem);
-
   }
 }
 
 async function hashFilesAndUpdateOutput() {
-
   numberOfFiles();
   listofFiles();
 
@@ -132,16 +125,18 @@ async function hashFilesAndUpdateOutput() {
   // Iterate over each file in the file select input
   for (const fileItem of file.files) {
     // Calculate its hash using the selected hash function
-    if ( file.files.length == 1 ){
+    if (file.files.length == 1) {
       outHTML += `${await fileHash(fileItem, selectedHashFunction)}`;
     } else {
-      outHTML += `${fileItem.name}    ${await fileHash(fileItem, selectedHashFunction)}<br>`;
+      outHTML += `${fileItem.name}    ${await fileHash(
+        fileItem,
+        selectedHashFunction
+      )}<br>`;
     }
   }
   output.innerHTML = outHTML;
 
   hideProcessingAnimation();
-
 }
 
 function updateHashFunction() {
