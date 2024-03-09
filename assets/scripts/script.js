ed5bfbec1361de8a2275b76d76b255775ad7ddcd
el-amine-404 <131032600+el-amine-404@users.noreply.github.com>
2024-03-09 11:40:30 +0100

    js: add size, number of files and a processing animation

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,5 +1,9 @@
 const output = document.querySelector("output");
 const file = document.getElementById("file");
+const fileList = document.getElementById("files-list");
+const numOfFiles = document.getElementById("num-of-files");
+const processingAnimation = document.getElementById("processingAnimation");
+
 let selectedHashFunction = "SHA-1"; // Default hash function
 
 // Run the hashing function when the user selects one or more files or changes the hash function
@@ -7,6 +11,15 @@ file.addEventListener("change", hashFilesAndUpdateOutput);
 document
   .getElementById("select-hash-function")
   .addEventListener("change", updateHashFunction);
+
+
+async function showProcessingAnimation() {
+  processingAnimation.style.display = "block";
+}
+
+async function hideProcessingAnimation() {
+  processingAnimation.style.display = "none";
+}
 
 async function fileHash(file, hashFunction) {
   const arrayBuffer = await file.arrayBuffer();
@@ -25,15 +38,80 @@ async function fileHash(file, hashFunction) {
   return hashAsString;
 }
 
+function numberOfFiles() {
+  const numFiles = file.files.length;
+
+  fileList.innerHTML = "";
+
+  if (numFiles==1){
+    numOfFiles.textContent = `${numFiles} File Selected`;
+  } else {
+    numOfFiles.textContent = `${numFiles} Files Selected`;
+    document.getElementById("label_result__file-hash").innerText = "Computed Hashes";
+  }
+}
+
+
+
+function listofFiles() {
+  for (f of file.files){
+    let listItem = document.createElement("li");
+    let fileName = f.name;
+
+    // Approximate to the closest prefixed unit
+    const units = [
+      "B",
+      "KiB",
+      "MiB",
+      "GiB",
+      "TiB",
+      "PiB",
+      "EiB",
+      "ZiB",
+      "YiB",
+    ];
+
+    const exponent = Math.min(
+      Math.floor(Math.log(f.size) / Math.log(1000)),
+      units.length - 1,
+    );
+    const approx = f.size / 1000 ** exponent;
+    const fileSize =
+      exponent === 0
+        ? `${f.size} bytes`
+        : `${approx.toFixed(3)} ${
+            units[exponent]
+          }`;
+          // } (${f.size} bytes)`;
+
+    listItem.innerHTML=`<div class="selected-file-icon-name"><i class="fa-solid fa-file-lines"></i><p>${fileName}</p></div><p>${fileSize}</p>`;
+
+    fileList.appendChild(listItem);
+
+  }
+}
+
 async function hashFilesAndUpdateOutput() {
+
+  numberOfFiles();
+  listofFiles();
+
   let outHTML = "";
+
+  showProcessingAnimation();
   // Iterate over each file in the file select input
   for (const fileItem of file.files) {
     // Calculate its hash using the selected hash function
-    // outHTML += `${fileItem.name}    ${await fileHash(fileItem, selectedHashFunction)}<br>`;
-    outHTML += `${await fileHash(fileItem, selectedHashFunction)}`;
+    if ( file.files.length == 1 ){
+      outHTML += `${await fileHash(fileItem, selectedHashFunction)}`;
+    } else {
+      outHTML += `${fileItem.name}    ${await fileHash(fileItem, selectedHashFunction)}<br>`;
+    }
   }
   output.innerHTML = outHTML;
+
+  hideProcessingAnimation();
+
 }
 
 function updateHashFunction() {
